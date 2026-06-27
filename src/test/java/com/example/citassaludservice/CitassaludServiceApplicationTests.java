195247ef7fbd1f2a8158f8e8867fedb07d71375d
Cristian <christianapolo98@gmail.com>
2026-06-27 08:42:38 -0500

    Initial commit from Specify template

---
+++ src/test/java/com/example/citassaludservice/CitassaludServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.citassaludservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitassaludServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
